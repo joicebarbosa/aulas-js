d7c971b6ed743120e1dab8e23ed060b808bd7300
Joice Barbosa <joicevbarbosa@gmail.com>
2021-12-11 01:26:10 -0300

    aulas-js

---
+++ js/main.js
@@ -0,0 +1,56 @@
+var d = new Date();
+alert(d);
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+
+
+
+
+
+var count;
+for(count=0; count <= 5; count++){
+  alert(count)
+
+
+
+/*var count = 0;
+while (count <= 5) {
+  console.log(count);
+  alert(count);
+  count++;
+}*/
+
+
+
+//var idade = prompt("Qual sua idade? ");
+//if (idade >= 18 ){
+  //alert("maior de idade");
+//}else{
+ // alert("menor de idade");
+//}
+
+//var fruta = {nome:"maça", cor:"vermelha"}
+//console.log(fruta)
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva"); //push, coloca um elemento na lista
+//lista.pop(); //pop, retira um elemento da lista
+//console.log(lista.length) - lengthe, lê a quantidade de elemento na lista 
+//console.log(lista.reverse()) - reverse, lê (tras) a os os elementos ao contrario
+//console.log(lista.toString());
+//console.log(lista.join(" - "))
+
+//console.log(lista[1])
+//alert (lista [1])
+
+//var nome = "Joice Barbosa"
+//var idade = 36
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos ");
+//alert() 
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.toLowerCase());
+//alert(frase.replace("Japão","Brasil"))
